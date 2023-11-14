02ebd1ac0f21687ea6084ced6c80d209e31a6881
Urmila111 <basneturmi777@gmail.com>
2023-11-14 17:39:31 +0545

    first commit

---
+++ tree.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b;
+	int area;
+	printf("Enter the length");
+	scanf("%d",&a);
+	printf("Enter the breadth");
+	scanf("%d",&b);
+	area= a*b;
+	printf("Area is %d",area);
+	getch();
+	
+}
---
+++ urm.c
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>Arithmetic operator Example</h1>
+<script type="text/javascript">
+var a=22;
+var b=5;
+ var sum=a+b;
+ var dif=a-b;
+document.write("Sum="+sum+"<br>");
+document.write("Difference="+diff+"<br>");
+</script>
+</body>
+</html>
